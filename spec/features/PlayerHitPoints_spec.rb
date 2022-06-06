e9b06f09f5a5c03ead7271603d3fd2b1dc43daf2
fridayshoes <one_amp@hotmail.com>
2022-06-06 12:31:27 +0100

    Small update to hit points test

--- spec/features/PlayerHitPoints_spec.rb
+++ spec/features/PlayerHitPoints_spec.rb
@@ -8,6 +8,6 @@ feature "Player 1" do
 
     # save_and_open_page # will save the web page and open the browser to display it
     
-    expect(page).to have_content 'Mittens: 60pts'
+    expect(page).to have_content 'Mittens: 60HP'
   end
 end
